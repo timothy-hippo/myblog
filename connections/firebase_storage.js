c3a745ce6407ff8f34ed785b08f8c58499008cfe
timothy <timothy80617@yahoo.com.tw>
2021-04-09 17:54:31 +0800

    修改CKEditor

--- connections/firebase_storage.js
+++ connections/firebase_storage.js
@@ -4,8 +4,8 @@ require("dotenv").config();
 const storage = new Storage({
   projectId: process.env.GCLOUD_PROJECT_ID,
   keyFilename: "./myblog-71876-firebase-adminsdk-eny5u-6e477da037.json",
-}); //2
+}); 
 
-const bucket = storage.bucket(process.env.GCLOUD_STORAGE_BUCKET_URL); //5
+const bucket = storage.bucket(process.env.GCLOUD_STORAGE_BUCKET_URL); 
 
 module.exports = bucket;
